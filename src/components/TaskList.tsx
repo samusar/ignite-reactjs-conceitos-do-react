0f5ce62eb180f527cfa5faca81f59d2cb1d3c926
Samuel Henrique <samu.henr@gmail.com>
2021-04-26 20:49:18 -0300

    Resolution test

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,32 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle !== "") {
+      setTasks(oldsValue => [...oldsValue, {
+        id: tasks.length,
+        title: newTaskTitle,
+        isComplete: false,
+      }]);
+  
+      setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(oldsValue => oldsValue.map(task => {
+      if (task.id === id) {
+        return {
+          ...task,
+          isComplete: !task.isComplete,
+        };
+      }
+
+      return task;
+    }))
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(oldsValue => oldsValue.filter(task => task.id !== id));
   }
 
   return (
